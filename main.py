3ed6c1fa4aacdf4121b77ae12f1c54f2d7376bd3
tuss4 <tuss4dbfn@gmail.com>
2019-03-02 03:17:27 -0600

    Initial commit.

---
+++ main.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+
+def run():
+    while True:
+        print("Omae wa mo shindeiru")
+        sleep(2)
+
+
+if __name__ == "__main__":
+    run()
---
+++ scrapper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+ADDR = "https://www.marvel.com/comics/calendar/week/2019-02-24?byZone=marvel_site_zone&offset=0&tab=comic&formatType=issue&isDigital=0&byType=date&dateStart=2019-02-24&dateEnd=2019-03-02&orderBy=release_date+desc&limit=300&count=41"
+
+
+def get_the_latest_titles():
+    page = urllib.request.urlopen(ADDR).read()
+    soup = BeautifulSoup(page, 'html.parser')
+    # titles = soup.findAll('div', attrs={'class': 'row-item-text'})
+    titles = soup.findAll('h5')
+    for title in titles:
+        print(title.text.strip())
+    return
+
+
+if __name__ == "__main__":
+    get_the_latest_titles()
