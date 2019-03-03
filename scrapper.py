e55d314bc56c749ba74eaf2d09629c2816d8d0f7
tuss4 <tuss4dbfn@gmail.com>
2019-03-03 02:33:57 -0600

    scraper's good to go, let's make a lambda!

--- scrapper.py
+++ scrapper.py
@@ -1,18 +1,38 @@
 from bs4 import BeautifulSoup
 import urllib.request
+from datetime import timedelta, date
 
 
-ADDR = "https://www.marvel.com/comics/calendar/week/2019-02-24?byZone=marvel_site_zone&offset=0&tab=comic&formatType=issue&isDigital=0&byType=date&dateStart=2019-02-24&dateEnd=2019-03-02&orderBy=release_date+desc&limit=300&count=41"
+DATE_FMT = "%Y-%m-%d"
+
+
+ADDR = "https://www.marvel.com/comics/calendar/week/{0}?byZone=marvel_site_zone&offset=0&tab=comic&formatType=issue&isDigital=0&byType=date&dateStart={1}&dateEnd={2}&orderBy=release_date+desc&limit=300&count=41"
+
+
+def get_sun_to_sat():
+    today = date.today()
+    dow_int = int(today.strftime('%w'))
+    sunday = None
+    if dow_int == 0:
+        sunday = date.today()
+    else:
+        sunday = date.today - timedelta(days=dow_int)
+    saturday = sunday + timedelta(days=6)
+    fmt_sun = sunday.strftime(DATE_FMT)
+    fmt_sat =  saturday.strftime(DATE_FMT)
+    return fmt_sun, fmt_sun, fmt_sat
 
 
 def get_the_latest_titles():
-    page = urllib.request.urlopen(ADDR).read()
+    url = ADDR.format(*get_sun_to_sat())
+    page = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(page, 'html.parser')
     titles = soup.findAll('h5')
-    for title in titles:
-        print(title.text.strip())
-    return
+    # for title in titles:
+    #     print(title.text.strip())
+    latest = [title.text.strip() for title in titles]
+    return latest
 
 
 if __name__ == "__main__":
-    get_the_latest_titles()
+    print(get_the_latest_titles())
